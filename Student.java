a971b6ff66f8854a3d7a5aa10539b22d760ea54b
Harlotte <45529303+Harlotte1998@users.noreply.github.com>
2021-11-25 20:20:22 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,79 +1,34 @@
-package HeiMa.a140_StudentManage;
+package HeiMa.a236_ArrayListJiHeCunChuXueShengDuiXIangSanZHongFangShiBianLian;
 
 /**
- * @Author Miracle Liuce
- * @Date 2021/10/31 12:59
- * @Version 1.0
-
-
- 学生类
+ * @author ：LC
+ * @date ： 2021/11/25 19:58
  */
-
 public class Student {
-
-    //学号
-    private String sid;
-    //姓名
+    private int age;
     private String name;
-    //年龄
-    private String age;
-    //居住地
-    private String address;
 
-
-
-    public  Student(){
-
+    public Student() {
     }
 
-    public Student(String sid , String name , String age , String address){
-        this.name=name;
-        this.sid=sid;
-        this.age=age;
-        this.address=address;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
-    //学生类 获得sid
-    public String getSid(){
-        return sid;
-    }
-
-    //学生类 输出sid
-    public void setSid(String sid){
-        this.sid=sid;
-    }
-
-    //学生类 获得name
-    public String getName(){
-        return name;
-    }
-
-    //学生类 输出name
-    public void setName(String name){
-        this.name=name;
-    }
-
-
-    //学生类 获得age
-    public String getAge(){
+    public int getAge() {
         return age;
     }
 
-    //学生类 输出age
-    public void setAge(String age){
-        this.age=age;
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    //学生类 获得address
-    public String getAddress(){
-        return address;
+    public String getName() {
+        return name;
     }
 
-    //学生类 输出address
-    public void setAddress(String address){
-        this.address=address;
+    public void setName(String name) {
+        this.name = name;
     }
-
 }
-
-
