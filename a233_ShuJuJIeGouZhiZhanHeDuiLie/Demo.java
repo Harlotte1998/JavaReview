81bdbb0e077a525cc1bfb15967b594bc44bdaeb9
Harlotte <45529303+Harlotte1998@users.noreply.github.com>
2021-11-30 20:41:26 +0800

    Add files via upload

---
+++ a233_ShuJuJIeGouZhiZhanHeDuiLie/Demo.java
@@ -0,0 +1,29 @@
+package HeiMa.a233_ShuJuJIeGouZhiZhanHeDuiLie;
+
+/**
+ * @author ：LC
+ * @date ： 2021/11/25 19:20
+
+2.7常见数据结构之栈
+
+            数据进入栈模型的过程称为:压/进栈
+            数据离开栈模型的过程称为:弹/出栈
+            栈是一种数据先进后出的模型
+
+            栈顶元素          数据D
+                            数据C
+                            数据B
+            栈底元素          数据A
+
+2.8常见数据结构之队列
+
+            数据从后端进入队列模型的过程称为:入队列
+            数据从前端离开队列模型的过程称为:出队列
+
+
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
